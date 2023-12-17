cd4b2c00fd40a2ccd0c5f477e092e8abdf3cd814
abdullah01 <arfathossen541@gmail.com>
2023-12-17 08:50:50 +0600

    database changed

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes");
-const connectDB = require("./db/db");
-const errorHandler = require("./middleware/errorMiddleware");
-const PORT = process.env.PORT || 3000;
-
-dotenv.config();
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Connect to MongoDB
-connectDB();
-
-// Middleware
+//middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/users", userRoutes);
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.rjnekog.mongodb.net/VegetablesShopBD?retryWrites=true&w=majority`;
 
-app.use(errorHandler);
-
-// Start the server
-app.get("/", (req, res) => {
-  res.send("MongoDB connected  is running");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Eco Bazar shop server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Eco Bazar shop is running on port ${port}`);
 });
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const { getUserProfile } = require("../controllers/userController");
-
-router.get("/profile", authMiddleware, getUserProfile);
-
-module.exports = router;
