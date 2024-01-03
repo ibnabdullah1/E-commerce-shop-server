0984bd8e9d9f0160efae1b2c39b49f56d87e1dc9
abdullah01 <arfathossen541@gmail.com>
2024-01-03 17:58:55 +0600

    Some Rest APIs implements

--- index.js
+++ index.js
@@ -22,6 +22,58 @@ const client = new MongoClient(MONGO_URI, {
 
 async function run() {
   try {
+    // Database collection
+    const usersCollection = client.db("VegetablesShopBD").collection("users");
+    const featuredProductsCollection = client
+      .db("VegetablesShopBD")
+      .collection("featuredProducts");
+    const productsCollection = client
+      .db("VegetablesShopBD")
+      .collection("products");
+    // Save user in database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const update = { $set: user };
+      const options = { upsert: true };
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) return res.send({ message: "User already exist" });
+      const result = await usersCollection.updateOne(query, update, options);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/featured-products", async (req, res) => {
+      const result = await featuredProductsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/categories", async (req, res) => {
+      const result = await productsCollection
+        .aggregate([
+          {
+            $group: {
+              _id: "$category",
+              quantity: {
+                $sum: 1,
+              },
+            },
+          },
+          {
+            $project: {
+              category: "$_id",
+              quantity: "$quantity",
+            },
+          },
+        ])
+        .toArray();
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
